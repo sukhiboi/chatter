ad46a7dc721c0882eaeb1ced5bfe6e6d5531b011
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-18 22:29:59 +0530

    Added intractions

--- client.js
+++ client.js
@@ -1,7 +1,17 @@
 const { Socket } = require('net');
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const socket = new Socket();
 const [, , host, username] = process.argv;
+console.clear();
+
+rl.setPrompt(`${username}> `);
 
 socket.setEncoding('utf8');
 socket.connect({
@@ -12,11 +22,15 @@ socket.connect({
 
 socket.on('connect', () => {
   socket.write(JSON.stringify({ username }));
-  process.stdin.on('data', data => {
+  process.stdout.write('Connected to server');
+  rl.prompt();
+  rl.on('line', data => {
     socket.write(data.toString());
+    rl.prompt();
   });
 });
 
 socket.on('data', data => {
   process.stdout.write(data);
+  rl.prompt();
 });
