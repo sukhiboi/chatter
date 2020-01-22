9ba4a7664acf0e8371d8011250afd15fd1c5f3ea
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-22 17:30:52 +0530

    Refactored request parser

--- v2/responses.js
+++ v2/responses.js
@@ -6,14 +6,15 @@ const parseRequest = function(request, address, port) {
   const [req, ...headerAndBody] = request.split('\n');
   const [method, path, protocol] = req.split(' ');
   const { headers, body } = seprateHeadersAndBody(headerAndBody);
-  const headersObj = {};
-  headers.forEach(header => {
+
+  const headersObj = headers.reduce((headersObj, header) => {
     const headerParts = header.split(': ');
     const headerName = headerParts[0];
     const headerValue = headerParts[1].replace('\r', '');
-
     headersObj[headerName] = headerValue;
+    return headersObj;
   });
+
   return {
     url: `${address}${path}`,
     method,
