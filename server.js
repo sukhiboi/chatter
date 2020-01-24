185611b508fafc55be2973f34aca9fbc98d8533c
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-24 23:24:48 +0530

    changed folder structure

--- server.js
+++ server.js
@@ -1,55 +1,23 @@
 const { Server } = require('net');
+const { handleRequest } = require('./lib/responseHandler');
 
 const server = new Server();
 const PORT = 8000;
 
-server.listen(PORT);
+server.listen(process.argv[2] || PORT);
 
 server.on('listening', () => {
-  const localAddress = server.address().port;
-  console.log(`Listening on port ${localAddress}`);
+  const { address, family, port } = server.address();
+  console.clear();
+  console.log(`\nListening on port ${address}:${port} with ${family}\n`);
 });
 
-const users = {};
-
 server.on('connection', socket => {
-  socket.once('data', request => {
-    const headers = JSON.parse(request);
-    const username = headers.username;
-    process.stdout.write(`${username} connected\n`);
-    users[username] = {
-      socket,
-      name: username
-    };
-
-    socket.on('close', () => {
-      delete users[username];
-      console.log(`${username} disconnected`)
-     
-      const allUsers = Object.keys(users);
-      allUsers.forEach(name => {
-          const user = users[name];
-          user.socket.write(`\nSERVER> ${username} disconnected\n`);
-      });
-    });
-
-    socket.on('data', message => {
-      const getSender = function() {
-        const sender = Object.keys(users).filter(user => {
-          return users[user].socket.remotePort == socket.remotePort;
-        });
-        return sender[0];
-      };
-
-      const sender = getSender();
-      const allUsers = Object.keys(users);
-      allUsers.forEach(name => {
-        if (name !== sender) {
-          const user = users[name];
-          const msg = message.toString();
-          user.socket.write(`\n${username}> ${msg}\n`);
-        }
-      });
-    });
+  socket.setEncoding('utf8');
+  socket.on('data', data => {
+    const socketDetails = `${socket.remoteAddress}:${socket.remotePort} using ${socket.remoteFamily}`;
+    const response = handleRequest(data, socketDetails);
+    socket.write(response);
+    socket.end();
   });
 });
