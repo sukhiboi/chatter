55d80169623533f713b4d89d32f0de3acb0c8dba
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-31 08:27:21 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-const { Server } = require('net');
-const { handleRequest } = require('./lib/responseHandler');
+const { Server } = require('http');
+const { serveStaticPage, notFound, registerUser } = require('./lib/handlers');
+const { bodyParser } = require('./lib/middlewares');
+const { App } = require('./lib/app');
+const app = new App();
 
-const server = new Server();
+app.use(bodyParser);
+app.get('/', serveStaticPage);
+app.post('/', registerUser);
+app.get('', notFound);
+
+
+const server = new Server(app.serve.bind(app));
 const PORT = 8000;
 
 server.listen(process.argv[2] || PORT);
-
-server.on('listening', () => {
-  const { address, family, port } = server.address();
-  console.clear();
-  console.log(`\nListening on port ${address}:${port} with ${family}\n`);
-});
-
-server.on('connection', socket => {
-  socket.setEncoding('utf8');
-  socket.on('data', data => {
-    const socketDetails = `${socket.remoteAddress}:${socket.remotePort} using ${socket.remoteFamily}`;
-    const response = handleRequest(data, socketDetails);
-    socket.write(response);
-    socket.end();
-  });
-});
